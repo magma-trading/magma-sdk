d2d24063675e95b04bfa03bd3b9b60b865222d3c
Qhashfoundation <hello@qhash.foundation>
2025-12-28 13:16:05 -0500

    .

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+go build -o ./bin/traderd	./cmd/traderd
+go build -o ./bin/ui		./cmd/ui
--- run-sdk.sh
+++ run-sdk.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # ui
-go run ./cmd/ui -addr localhost:8020 -traderd-sock /tmp/traderd.sock >ui.log 2>&1 &
+./bin/ui -addr localhost:8020 -traderd-sock /tmp/traderd.sock >ui.log 2>&1 &
 
 # server
-go run ./cmd/traderd --allowlist-file=./allowlist.txt
+./bin/traderd --allowlist-file=./allowlist.txt
 
